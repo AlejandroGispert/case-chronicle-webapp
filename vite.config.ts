1bbe82b9dcb63c0d1f918d960527ccb369f7be83
AlejandroGispert <alejandrobusiness2022@gmail.com>
2025-05-14 20:01:31 +0200

    fix: netlify 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
-    port: 8080,
+    port: 3000,
+    strictPort: true,
+    host: true
   },
   plugins: [
     react(),
