4a1a9247084ab256330959ea12020af3999b0b5a
Alejandro Gispert <alejandrobusiness2022@gmail.com>
2025-05-16 08:28:57 +0200

    fix: change logic

--- src/backend/controllers/caseAccessController.ts
+++ src/backend/controllers/caseAccessController.ts
@@ -2,6 +2,6 @@ import { caseAccessModel } from "../models/caseAccessModel";
 
 export const caseAccessController = {
   async fetchPublicCase(code: string) {
-    return await caseAccessModel.getCaseWithRelationsByAccessCode(code);
+    return await caseAccessModel.getCaseByAccessCode(code);
   }
 };
